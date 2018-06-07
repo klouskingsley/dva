6691b9b761399988eef67d4440fadbc0b7d08799
harry <harry_tse@163.com>
2018-06-07 13:37:29 +0800

    add comment

--- packages/dva-core/src/prefixNamespace.js
+++ packages/dva-core/src/prefixNamespace.js
@@ -2,11 +2,17 @@ import warning from 'warning';
 import { isArray } from './utils';
 import { NAMESPACE_SEP } from './constants';
 
+/**
+ *
+ * @param {object} obj reducers/effects
+ * @param {string} namespace model's namespace
+ * @param {string} type typeof the `obj`: reducer|effect
+ */
 function prefix(obj, namespace, type) {
   return Object.keys(obj).reduce((memo, key) => {
     warning(
       key.indexOf(`${namespace}${NAMESPACE_SEP}`) !== 0,
-      `[prefixNamespace]: ${type} ${key} should not be prefixed with namespace ${namespace}`,
+      `[prefixNamespace]: ${type} ${key} should not be prefixed with namespace ${namespace}`
     );
     const newKey = `${namespace}${NAMESPACE_SEP}${key}`;
     memo[newKey] = obj[key];
@@ -14,12 +20,13 @@ function prefix(obj, namespace, type) {
   }, {});
 }
 
+/**
+ *
+ * @param {Model} model the model to be prefixed
+ * @returns {Model} prefixedmodel
+ */
 export default function prefixNamespace(model) {
-  const {
-    namespace,
-    reducers,
-    effects,
-  } = model;
+  const { namespace, reducers, effects } = model;
 
   if (reducers) {
     if (isArray(reducers)) {
